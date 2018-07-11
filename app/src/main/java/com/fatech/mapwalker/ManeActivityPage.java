c5b83e1f6841475e28a33bf6d87d3e2addbfffee
Dima Litvinov <DmLtvnv@yandex.ru>
2018-07-12 02:16:51 +0300

    Dirty second version 1.0

--- app/src/main/java/com/fatech/mapwalker/ManeActivityPage.java
+++ app/src/main/java/com/fatech/mapwalker/ManeActivityPage.java
@@ -17,45 +17,58 @@ import java.io.IOException;
 
 public class ManeActivityPage extends AppCompatActivity {
 
+    private Button toPage2Button;
+    private Intent intentMapPage;
+    private int distance;
+    private LatLng positionOnMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mane_page);
-        Button toPage2Button = (Button) findViewById(R.id.toPage2Button);
+        toPage2Button = (Button) findViewById(R.id.toPage2Button);
         toPage2Button.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-                toPage2();
-                }
+            public void onClick(View v) {
+                valueFrominputs();
+            }
         });
     }
 
-    private void toPage2() {
-        Intent intentMapPage = new Intent(this,MapActivity.class);
-            try {
-                EditText positionEditText = (EditText) findViewById(R.id.position);
-                EditText distanceEditText = (EditText) findViewById(R.id.distance);
-                int distance = Integer.parseInt(distanceEditText.getText().toString());
-                String position = positionEditText.getText().toString();
-                LatLng adress = searchResultsPosition(position);
-            intentMapPage.putExtra("position", adress);
-            intentMapPage.putExtra("distance", distance);
-            }
-            catch(Exception e){
-                Toast toast = Toast.makeText(this, "Error 1 \n" + e,Toast.LENGTH_LONG);
-                toast.show();
-            }
-        startActivity(intentMapPage);
+    private void valueFrominputs() {
+        try {
+            EditText positionEditText = (EditText) findViewById(R.id.position);
+            EditText distanceEditText = (EditText) findViewById(R.id.distance);
+            distance = Integer.parseInt(distanceEditText.getText().toString());
+            String location = positionEditText.getText().toString();
+            searchPosition(location);
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, "Не заполнены поля", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
-    private LatLng searchResultsPosition(String location){
-        Geocoder geocoder = new Geocoder(this);
+    private void searchPosition(String location) {
         try {
+            Geocoder geocoder = new Geocoder(this);
             Address address = geocoder.getFromLocationName(location, 1).get(0);
-            LatLng positionOnMap = new LatLng(address.getLatitude(), address.getLongitude());
-            return positionOnMap;
+            positionOnMap = new LatLng(address.getLatitude(), address.getLongitude());
+            toPage2();
         } catch (IOException e) {
-            e.printStackTrace();
+            Toast toast = Toast.makeText(this, "Не нашли место на карте", Toast.LENGTH_LONG);
+            toast.show();
         }
-        return null;
     }
+
+    private void toPage2() {
+        try {
+            intentMapPage = new Intent(this, MapActivity.class);
+            intentMapPage.putExtra("position", positionOnMap);
+            intentMapPage.putExtra("distance", distance);
+            startActivity(intentMapPage);
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, "Не получилось открыть карты", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
 }
--- app/src/main/java/com/fatech/mapwalker/MapActivity.java
+++ app/src/main/java/com/fatech/mapwalker/MapActivity.java
@@ -5,6 +5,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -13,21 +17,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapActivity extends Activity implements OnMapReadyCallback{
+import java.util.ArrayList;
+import java.util.List;
 
-    public static GoogleMap googleMap;
+public class MapActivity extends Activity implements OnMapReadyCallback, RoutingListener {
+
+    private GoogleMap googleMap;
+    private Bundle argumentsFromMainPage;
+    private LatLng address;
+    private List<Polyline> polylines;
+    private static final int[] COLORS = new int[]{R.color.primary_dark_material_light};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_map);
+            polylines = new ArrayList<>();
             MapFragment mapFragment = (MapFragment) getFragmentManager()
                     .findFragmentById(R.id.mapView);
             mapFragment.getMapAsync(this);
-        }
-        catch (Exception e){
-            Toast toast = Toast.makeText(this, "Error 2 \n "+ e.toString(),Toast.LENGTH_LONG);
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, "Не смогли открыть карты", Toast.LENGTH_LONG);
             toast.show();
         }
     }
@@ -36,23 +48,72 @@ public class MapActivity extends Activity implements OnMapReadyCallback{
     public void onMapReady(GoogleMap map) {
         googleMap = map;
         try {
-            Bundle argumentsFromMainPage = getIntent().getExtras();
-            LatLng address = (LatLng) argumentsFromMainPage.get("position");
-//            googleMap.addMarker(new MarkerOptions().position(address));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(address, 18));
-            PolylineOptions rectOptions = new PolylineOptions()
-                    .add(address)
-                    .add(new LatLng(55.751244, 37.618423)); // Closes the polyline.
-            // Set the rectangle's color to red
-            rectOptions.color(Color.RED);
-
-            // Get back the mutable Polyline
-            Polyline polyline = googleMap.addPolyline(rectOptions);
-        }
-        catch (Exception e){
-            Toast toast = Toast.makeText(this, "Error 2 \n "+ e.toString(),Toast.LENGTH_LONG);
+            argumentsFromMainPage = getIntent().getExtras();
+            address = (LatLng) argumentsFromMainPage.get("position");
+            moveCamera();
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, "Ошибка определения локации" + e.toString(), Toast.LENGTH_LONG);
             toast.show();
         }
     }
 
+    private void moveCamera() {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(address, 18));
+        makePolyline();
+    }
+
+    private void makePolyline() {
+        LatLng end = new LatLng(55.751244, 37.618423);
+        Routing routing = new Routing.Builder()
+                .travelMode(Routing.TravelMode.WALKING)
+                .withListener(this)
+                .waypoints(address,end)
+                .build();
+        routing.execute();
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+        if(polylines.size()>0) {
+            for (Polyline poly : polylines) {
+                poly.remove();
+            }
+        }
+
+        polylines = new ArrayList<>();
+        //add route(s) to the map.
+        for (int i = 0; i <route.size(); i++) {
+            //In case of more than 5 alternative routes
+            int colorIndex = i % COLORS.length;
+            PolylineOptions polyOptions = new PolylineOptions();
+            polyOptions.color(getResources().getColor(COLORS[colorIndex]));
+            polyOptions.width(10 + i * 3);
+            polyOptions.addAll(route.get(i).getPoints());
+            Polyline polyline = googleMap.addPolyline(polyOptions);
+            polylines.add(polyline);
+            Toast.makeText(getApplicationContext(),"Путь "+ (i+1) +": расстояние - "+ route.get(i).getDistanceValue()/1000 + " км",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        if(e != null) {
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this, "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //Not used method
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    //Not used method
+    @Override
+    public void onRoutingCancelled() {
+
+    }
 }
